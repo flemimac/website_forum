d7b5d30904ff049fb33c28a9a698f25d498d568c
flemimac <64695348+flemimac@users.noreply.github.com>
2024-01-26 12:46:14 +0200

    Add topline and adaptive for mobile/PC

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,30 @@
-$(function() {
+$(function () {
+	$('.logo-litera').each(function () {
+		var ths = $(this)
+		ths.html(ths.html().replace('O', '<span>O</span>'))
+	})
 
-	// Custom JS
+	$('.search').click(function () {
+		$('.search-field').stop().slideToggle()
+		$('.search-field input[type=text').focus()
+	})
 
-});
+	$(document)
+		.keyup(function (e) {
+			if (e.keyCode == 27) {
+				$('.search-field').slideUp()
+			}
+		})
+		.click(function () {
+			$('.search-field').slideUp()
+		})
+	$('.search-wrap').click(function (e) {
+		e.stopPropagation()
+	})
+
+	$('.topline').after('<div class="mobile-menu d-lg-none">')
+	$('.top-menu').clone().appendTo('.mobile-menu')
+	$('.mobile-menu-button').click(function () {
+		$('.mobile-menu').stop().slideToggle()
+	})
+})
