e7dc756b9186873c4b4987d137d4e9a95541209b
Alexander <64695348+flemimac@users.noreply.github.com>
2024-04-09 17:20:54 +0200

    layout end

--- app/js/common.js
+++ app/js/common.js
@@ -1,41 +1,44 @@
-$(function () {
-	$('.logo-litera').each(function () {
-		var ths = $(this)
-		ths.html(ths.html().replace('O', '<span>O</span>'))
-	})
+$(function() {
 
-	$('.search').click(function () {
-		$('.search-field').stop().slideToggle()
-		$('.search-field input[type=text').focus()
-	})
+	$('.logo-litera').each(function() {
+		var ths = $(this);
+		ths.html(ths.html().replace('O', '<span>O</span>'));
+	});
 
-	$(document)
-		.keyup(function (e) {
-			if (e.keyCode == 27) {
-				$('.search-field').slideUp()
-			}
-		})
-		.click(function () {
-			$('.search-field').slideUp()
-		})
-	$('.search-wrap').click(function (e) {
-		e.stopPropagation()
-	})
+	$('.search').click(function() {
+		$('.search-field').stop().slideToggle();
+		$('.search-field input[type=text]').focus();
+	});
 
-	$('.topline').after('<div class="mobile-menu d-lg-none">')
-	$('.top-menu').clone().appendTo('.mobile-menu')
-	$('.mobile-menu-button').click(function () {
-		$('.mobile-menu').stop().slideToggle()
-	})
-
-	$('.col-item').hover(
-		function () {
-			ths = $(this)
-			lnk = ths.closest('.col-item').find('h4 a')
-			lnk.addClass('hover')
-		},
-		function () {
-			lnk.removeClass('hover')
+	$(document).keyup(function(e) {
+		if (e.keyCode == 27) {
+			$('.search-field').slideUp();
 		}
-	)
-})
+	}).click(function() {
+		$('.search-field').slideUp();
+	});
+	$('.search-wrap').click(function(e) {
+		e.stopPropagation();
+	});
+
+	$('.top-line').after('<div class="mobile-menu d-lg-none">');
+	$('.top-menu').clone().appendTo('.mobile-menu');
+	$('.mobile-menu-button').click(function() {
+		$('.mobile-menu').stop().slideToggle();
+	});
+
+	$('.col-item').hover(function() {
+		ths = $(this);
+		lnk = ths.closest('.col-item').find('h4 a');
+		lnk.addClass('hover');
+	}, function() {
+		lnk.removeClass('hover');
+	});
+
+	$("body").prognroll({
+		height: 3,
+		color: "#ec1c1c",
+		custom: false
+	});
+
+});
